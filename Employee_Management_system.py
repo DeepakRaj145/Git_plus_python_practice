f0ade0319461e35ae7f3bfe5e62aa560e7e983b6
DeepakRaj <deepakraj140498@gmail.com>
2026-07-01 17:28:45 +0530

    Feat : Employee Detail Added

---
+++ Employee_Management_system.py
@@ -0,0 +1,18 @@
+class Employee:
+    def __init__(self, emp_id, emp_name, salary):
+        self.emp_id = emp_id
+        self.emp_name = emp_name
+        self.salary = salary
+        
+    def display_employee_details(self):
+        print(f'Employee Id : {self.emp_id}')
+        print(f'Employee Name : {self.emp_name}')
+        print(f'Employee Salary : {self.salary}')
+        
+emp1 = Employee(1, 'Deepak', 20000)
+emp2 = Employee(2, 'Sarath', 40000)
+
+emp1.display_employee_details()
+emp2.display_employee_details()
+
+        
