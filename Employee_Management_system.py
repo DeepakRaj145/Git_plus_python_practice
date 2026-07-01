1737b788e5d9aded83284584a71d2de33d12f2fc
DeepakRaj <deepakraj140498@gmail.com>
2026-07-01 18:31:14 +0530

    Feat: Added Annual Salary and Increment

--- Employee_Management_system.py
+++ Employee_Management_system.py
@@ -5,14 +5,26 @@ class Employee:
         self.salary = salary
         
     def display_employee_details(self):
-        print(f'Employee Id : {self.emp_id}')
+        print(f'Employee_Id : {self.emp_id}')
         print(f'Employee Name : {self.emp_name}')
         print(f'Employee Salary : {self.salary}')   
-        
+    
+    def annual_salary(self):
+        print(f'Annual Salary : {self.salary * 12}')
+    
+    def annual_increment(self, percentage):
+        increment = self.salary * percentage/100
+        self.salary += increment
+        print(f'Annual Increament Salary {int(self.salary *12)}')
 emp1 = Employee(1, 'Deepak', 20000)
 emp2 = Employee(2, 'Sarath', 40000)
 
 emp1.display_employee_details()
+emp1.annual_salary()
+emp1.annual_increment(20)
+
 emp2.display_employee_details()
+emp2.annual_salary()
+emp2.annual_increment(10)
 
         
